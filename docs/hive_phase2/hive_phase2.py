812291388594da506b6add79ddec17972f121ab3
John Owens <jowens@ece.ucdavis.edu>
2021-08-23 15:58:17 -0700

    added version summary

--- docs/hive_phase2/hive_phase2.py
+++ docs/hive_phase2/hive_phase2.py
@@ -16,6 +16,7 @@ files = sorted(
             (
                 f.startswith("hive_")
                 and f.endswith(".md")
+                and f != "hive_version_summary.md"
                 and f != "hive_datasets.md"
                 and f != "hive_template_phase2.md"
                 and f != "hive_phase2_summary.md"
@@ -114,7 +115,7 @@ table_tempfile = tempfile.NamedTemporaryFile(mode="w+", suffix=".md")
 table_tempfile.write("\n\n# Tables of Performance Results\n\n")
 
 # list of input files
-mdfiles = ["hive_phase2_summary.md", "hive_run_apps_phase2.md", "hive_forall_phase2.md"]
+mdfiles = ["hive_version_summary.md", "hive_phase2_summary.md", "hive_run_apps_phase2.md", "hive_forall_phase2.md"]
 for file in files:
     mdfiles.append(file)
     # format is hive_X_phase2.md
